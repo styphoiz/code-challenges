d04a4e8cc7762f17917769e414d995fd5fec7644
styphoiz <styphoiz@gmail.com>
2020-11-08 11:59:03 +0200

    Make task 10 readable, added different approach to solve problem

--- task10.py
+++ task10.py
@@ -1,7 +1,7 @@
 def print_vowel(text):
     output = ""
     for s in text:
-        if(s=="a"or s=="e"or s=="i"or s=="o"or s=="u"or s=="A"or s=="E"or s=="I"or s=="O"or s=="U"):
+        if(s.lower()=="a"or s.lower()=="e"or s.lower()=="i"or s.lower()=="o"or s.lower()=="u"):
             print(s)
             if(output==""):
                 output = s
@@ -9,5 +9,20 @@ def print_vowel(text):
                 output += " " + s
     return output
 
+def print_vowel_alternate(text):
+        vowels = "aeiou"
+        output = ""
+        for s in text:
+            for v in vowels:
+                if(s.lower()==v):
+                    print(s)
+                    if(output==""):
+                        output = s
+                    else:
+                        output += " " + s
+        return output
+
+
 print(print_vowel("HellO, this Is a vowEl test to identify >> A a E e I i O o U u"))
+print(print_vowel_alternate("HellO, this Is a vowEl test to identify >> A a E e I i O o U u"))
 
