62705666a140cb9f61e170ed956c3eb0b6e8dfc1
styphoiz <styphoiz@gmail.com>
2020-11-08 11:50:52 +0200

    Make task 8 more user friendly

--- task8.py
+++ task8.py
@@ -2,11 +2,19 @@ def time(num):
     hour = num//60
     minute = num % 60
     if(hour>1):
-        return str(hour) + " hours, " + str(minute) + " minutes"
+        if(minute>1):
+            return str(hour) + " hours, " + str(minute) + " minutes"
+        else:
+            return str(hour) + " hours, " + str(minute) + " minute"
     else:
-        return str(hour) + " hour, " + str(minute) + " minutes"
+            if(minute>1):
+                return str(hour) + " hour, " + str(minute) + " minutes"
+            else:
+                return str(hour) + " hour, " + str(minute) + " minute"
 
 #test
 print(time(10))
 print(time(100))
 print(time(200))
+print(time(61))
+print(time(121))
